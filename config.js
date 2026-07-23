71ba4a3846502a00e5aa3d0f42aae9a64e390a3f
alpdilgen <info@anova.bg>
2026-07-23 15:01:14 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAxp9Tj6kFat3a_EcWtwZ5w_cR0cyIqbW0",
+  authDomain: "worldcupcards-33969.firebaseapp.com",
+  databaseURL: "https://worldcupcards-33969-default-rtdb.firebaseio.com",
+  projectId: "worldcupcards-33969",
+  storageBucket: "worldcupcards-33969.firebasestorage.app",
+  messagingSenderId: "470743374514",
+  appId: "1:470743374514:web:6265aad191f3457f8b87ab"
+};
